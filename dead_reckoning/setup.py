1adfeb1514f30728234c199c8dd0ab30d99cfc4e
Anders Smitterberg <jasmitte@mtu.edu>
2026-02-01 11:39:03 -0500

    initial commit

---
+++ dead_reckoning/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'dead_reckoning'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Anders Smitterberg',
+    maintainer_email='jasmitte@mtu.edu',
+    description='TODO: Package description',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'estimator = dead_reckoning.estimator_node:main'
+        ],
+    },
+)
